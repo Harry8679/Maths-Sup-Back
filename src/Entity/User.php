41220ea054579720e9f7cafe5f2eeb03e788eb3a
Harry EMARH <emarh.harry.code@gmail.com>
2025-08-07 14:18:17 +0200

    User updated

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -31,6 +31,33 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastName = null;
+
+    #[ORM\Column]
+    private ?bool $isEmailVerified = null;
+
+    #[ORM\Column]
+    private ?bool $isValidatedByAdmin = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $gender = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $promoYear = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $phoneNumber = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $photo = null;
+
+    #[ORM\Column]
+    private ?bool $isPhoneIsVisible = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -110,5 +137,113 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function eraseCredentials(): void
     {
         // @deprecated, to be removed when upgrading to Symfony 8
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): static
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): static
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function isEmailVerified(): ?bool
+    {
+        return $this->isEmailVerified;
+    }
+
+    public function setIsEmailVerified(bool $isEmailVerified): static
+    {
+        $this->isEmailVerified = $isEmailVerified;
+
+        return $this;
+    }
+
+    public function isValidatedByAdmin(): ?bool
+    {
+        return $this->isValidatedByAdmin;
+    }
+
+    public function setIsValidatedByAdmin(bool $isValidatedByAdmin): static
+    {
+        $this->isValidatedByAdmin = $isValidatedByAdmin;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(string $gender): static
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getPromoYear(): ?string
+    {
+        return $this->promoYear;
+    }
+
+    public function setPromoYear(string $promoYear): static
+    {
+        $this->promoYear = $promoYear;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber(?string $phoneNumber): static
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?string $photo): static
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function isPhoneIsVisible(): ?bool
+    {
+        return $this->isPhoneIsVisible;
+    }
+
+    public function setIsPhoneIsVisible(bool $isPhoneIsVisible): static
+    {
+        $this->isPhoneIsVisible = $isPhoneIsVisible;
+
+        return $this;
     }
 }
